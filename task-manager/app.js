21444760c05430f49c25b27c175be92e909fffb5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-09-17 20:37:20 +0600

    Task manager complete

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
-app.get('/hello', (req, res) => {res.send('Task Manager App')})
 
 app.use('/api/v1/tasks', tasks)
 
-const port = 3000
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-app.listen(port, console.log(`server listening on port ${port}...`));
+const port = process.env.PORT || 3000
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server listening on port ${port}...`));
+    } catch(error) {
+        console.log(error);
+        
+    }
+}
+
+start()

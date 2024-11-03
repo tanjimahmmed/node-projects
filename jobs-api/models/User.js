20fe229f29b0f2440a06857a1bde15acd6b05e2d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-03 16:21:51 +0600

    jobs api

---
+++ jobs-api/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please provide name'],
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide name'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide valid email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide password'],
+        minlength: 6
+    },
+})
+
+module.exports = mongoose.model('User', UserSchema)

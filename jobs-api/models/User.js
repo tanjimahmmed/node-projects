e668b7afc11ee1748c40d8e3d0f4d3db0afe6ee5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-04 23:00:10 +0600

    add validation error

--- jobs-api/models/User.js
+++ jobs-api/models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'please provide name'],
+        required: [true, 'please provide email'],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide valid email'],
         unique: true
     },
